7a76bc4313eb7131ab7a9cc9a3536ae5b94ac747
siddhantprateek <siddhantprateek@gmail.com>
2022-06-25 02:10:25 +0530

    header component

---
+++ client/components/header/header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../../styles/Home.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className="logo">
+        <h1>NFQ 🏳️‍🌈</h1>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
