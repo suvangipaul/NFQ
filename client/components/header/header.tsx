1960dd03a8416793194c5d019d8d47241b17c87d
siddhantprateek <siddhantprateek@gmail.com>
2022-06-25 18:13:09 +0530

    auth0 header

--- client/components/header/header.tsx
+++ client/components/header/header.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0";
 
 const Header = () => {
+  const { user, isLoading } = useUser();
   return (
     <div className={styles.header}>
       <div className="logo">
@@ -12,16 +14,21 @@ const Header = () => {
       </div>
       <div>
         <ul className="nav-option">
-          <li>
-            <Link href="/explore">
-              Find your Match
-            </Link>
-          </li>
-          <li>
-            <Link href="/login">
-              Sign Up | Log In
-            </Link>
-          </li>
+          { user && (
+            <li>
+              <Link href="/explore">Find your Match</Link>
+            </li>
+          )}
+          {!user && !isLoading && (
+            <li>
+              <Link href="/api/auth/login">Sign Up | Log In</Link>
+            </li>
+          )}
+          {user && (
+            <li>
+              <Link href="/api/auth/logout">Log Out</Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
